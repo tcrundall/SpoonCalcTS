35c11ba1e5ec779a5edba2fe317f65c71f66c9ff
Timothy Crundall <tim.crundall@gmail.com>
2023-12-22 16:50:27 +1100

    Create sqlite database
    Following example found here:
    https://github.com/expo/examples/blob/master/with-sqlite/App.js

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SQLite from 'expo-sqlite';
 import HomeScreen from './screens/home';
 import LogsScreen from './screens/logs';
 import ActivityScreen from './screens/activity';
 import SymptomsScreen from './screens/symptoms';
 import DataScreen from './screens/data';
 import PlotsScreen from './screens/plots';
+import { useEffect } from 'react';
 
 
 const Stack = createNativeStackNavigator()
+const db = SQLite.openDatabase("spooncalc-rn.db");
 
 export default function App() {
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "createt table if not exists items (id integer primary key not null, done int, value text);"
+      );
+    });
+    console.log("Created db");
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
