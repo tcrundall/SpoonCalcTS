26e4a119c7c4eacceef4cc7fffb96c2541c87956
Timothy Crundall <tim.crundall@gmail.com>
2023-12-23 20:53:19 +1100

    Add dummy database

--- App.js
+++ App.js
@@ -17,10 +17,21 @@ export default function App() {
   useEffect(() => {
     db.transaction((tx) => {
       tx.executeSql(
-        "createt table if not exists items (id integer primary key not null, done int, value text);"
+        "create table if not exists items (id integer primary key not null, done int, value int);"
       );
     });
-    console.log("Created db");
+    console.log("Created table");
+  }, []);
+
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        console.log("Entering add transaction");
+        tx.executeSql("insert into items (done, value) values (0, 10);", []);
+        tx.executeSql("select * from items;", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      });
   }, []);
 
   return (
