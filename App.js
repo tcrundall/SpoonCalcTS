d9a5e2dda1c98f662502e7a3ce48a5d9a1b25da3
Timothy Crundall <tim.crundall@gmail.com>
2023-12-24 15:18:32 +1100

    Remove unnecessary dummy sqlite usage

--- App.js
+++ App.js
@@ -6,42 +6,14 @@ import ActivityScreen from './screens/activity';
 import SymptomsScreen from './screens/symptoms';
 import DataScreen from './screens/data';
 import PlotsScreen from './screens/plots';
-import { useEffect } from 'react';
-import Database from './database';
 
 
 const Stack = createNativeStackNavigator()
-const db = Database.getConnection();
 
 export default function App() {
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "create table if not exists items (id integer primary key not null, done int, value int);"
-      );
-      tx.executeSql("insert into items (done, value) values (0, 10);", []);
-      tx.executeSql("select * from items;", [], (_, { rows }) =>
-        console.log(`Current items: ${JSON.stringify(rows)}`)
-      );
-    });
-    console.log("Created table");
-  }, []);
-
-  useEffect(() => {
-    db.transaction(
-      (tx) => {
-        console.log("Entering add transaction");
-        tx.executeSql("insert into items (done, value) values (0, 10);", []);
-        tx.executeSql("select * from items;", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-        );
-        console.log("Leaving add transaction");
-      });
-  }, []);
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Logs">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
