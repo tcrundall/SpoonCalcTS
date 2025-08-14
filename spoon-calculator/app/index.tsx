594bd71731ddc6bc6aeb63d257cea8691f5f0106
Timothy Crundall <tim.crundall@gmail.com>
2025-08-14 17:08:53 +0200

    Add a simple (useless) button

--- spoon-calculator/app/index.tsx
+++ spoon-calculator/app/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from "react-native-elements";
 import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Spoon!</Text>
+      <Button>Some thing</Button>
       <Link href="/activity">View activity</Link>
       <Link href="/data">View data</Link>
       <Link href="/logs">View logs</Link>
