ad30830c5dba8ed633ad3a6ac997018760d0815f
Timothy Crundall <tim.crundall@gmail.com>
2025-09-14 16:48:29 +0200

    SC10: Unit test activity table creation

--- spoon-calculator/storage/__tests__/database.test.ts
+++ spoon-calculator/storage/__tests__/database.test.ts
@@ -1,9 +1,17 @@
-import { initialiseDatabase } from "../database.native";
-import { expect, describe, vi, it } from "vitest";
+import {
+  createActivitiesTable,
+  initialiseDatabase,
+  saveActivity,
+} from "../database.native";
+import { expect, describe, vi, it, beforeEach } from "vitest";
 
 const mocks = vi.hoisted(() => {
   return {
     execAsync: vi.fn(),
+    withTransactionAsync: (func: any) => {
+      return func();
+    },
+    getAllAsync: () => [],
   };
 });
 
@@ -11,19 +19,28 @@ vi.mock("expo-sqlite", () => ({
   openDatabaseSync: () => {
     return {
       execAsync: mocks.execAsync,
+      withTransactionAsync: mocks.withTransactionAsync,
+      getAllAsync: mocks.getAllAsync,
     };
-  },
-  initialiseDatabase: () => {
-    console.log("Mocked initialise database!");
   },
 }));
 
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
 describe("database", () => {
   it("creates a table if not exists upon initialisation", async () => {
-    console.log("Hello from test");
     await initialiseDatabase();
     expect(mocks.execAsync).toHaveBeenCalledExactlyOnceWith(
       expect.stringContaining("CREATE TABLE IF NOT EXISTS"),
+    );
+  });
+
+  it("creates activity table if not exists", async () => {
+    await createActivitiesTable();
+    expect(mocks.execAsync).toHaveBeenCalledExactlyOnceWith(
+      expect.stringContaining("create table if not exists activities"),
     );
   });
 });
